dbcae515f3a86ea77f8dbcf023e4c9791ddfc953
jacob <jacobdereje@gmail.com>
2022-09-28 16:38:16 -0500

    rectangle, tri

--- src/Circle.java
+++ src/Circle.java
@@ -3,12 +3,20 @@ public double x_position;
 public double y_position;
 public double height;
 
-Circle(double x_position, double y_position, double height){
+public Circle(double x_position, double y_position, double height){
     this.x_position = x_position;
     this.y_position = y_position;
     this.height = height;
 }
-public void circumfrance(){
+public void perimeter(){
+
+
+}
+public void area(){
+
+
+}
+public void setColor(String color){
 
 
 }
