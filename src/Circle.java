4dd8ed70334a7f4aa9cf3022bf80dc140a9dbc9b
jacob <jacobdereje@gmail.com>
2022-09-28 16:29:45 -0500

    rectangle, tri

--- src/Circle.java
+++ src/Circle.java
@@ -3,6 +3,14 @@ public double x_position;
 public double y_position;
 public double height;
 
-public static circumfrance()
+Circle(double x_position, double y_position, double height){
+    this.x_position = x_position;
+    this.y_position = y_position;
+    this.height = height;
+}
+public void circumfrance(){
+
+
+}
     }
 
