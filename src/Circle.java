bbba3f4b706660b4390c1b1620820690dc98c0c1
jacob <jacobdereje@gmail.com>
2022-09-28 16:23:06 -0500

    rectangle, tri

--- src/Circle.java
+++ src/Circle.java
@@ -1,7 +1,8 @@
 public class Circle {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("check");
-        System.out.println("it worked!!");
+public double x_position;
+public double y_position;
+public double height;
+
+public static circumfrance()
     }
-}
+
