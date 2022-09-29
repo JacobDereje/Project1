5c0605247900f1a97b953405ccf0847daff18a30
jacob <jacobdereje@gmail.com>
2022-09-29 15:52:23 -0500

    circ

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,11 +1,29 @@
+import javafx.beans.binding.DoubleExpression;
+
 public class Rectangle {
 public double x_position;
 public double y_position;
 public double height;
 public String Color;
+public double Pos;
+public double Width;
+
+public Perimeter(){
+    double perimeter;
+    perimeter = 2 * setHeight();
+}
 
 public void setColor(String color){
     this.Color = Color;
+}
+public void setPos(Double Pos){
+    this.Pos = Pos;
+}
+public void setHeight(double height){
+    this.height = height;
+}
+public void setWidth(double Width){
+    this.Width = Width;
 }
 
 }
